f0584e338723787f95a53e60aadb5c7bccab5e0d
latinology <90052941+latinology@users.noreply.github.com>
2021-09-18 15:05:40 -0400

    Fix elision and hexameter bugs

--- src/scanner.swift
+++ src/scanner.swift
@@ -21,9 +21,9 @@ public struct Scanner {
     public typealias Syllable = Syllabifier.Syllable
     
     public static func scan(line: [String], elide: Bool) -> [[Syllable]] {
-        // Convert words to syllables 
-        return scan(syllables: line.map { 
-            Syllabifier.syllabify(word: $0) 
+        // Convert words to syllables
+        return scan(syllables: line.map {
+            Syllabifier.syllabify(word: $0)
         }, elide: elide)
     }
     
@@ -32,12 +32,12 @@ public struct Scanner {
         let result = syllables.reduce(into: [[Syllable]]()) {
             if let last = $0.last?.last, !(last.isLong ?? false) {
                 // If we have a case of '-c c-', then the former syllable becomes long by position
-                if last.value.last.map { Syllabifier.isConsonant($0) } ?? false,
-                   $1.first.map { Syllabifier.startsWithScanConsonant($0.value) } ?? false {
+                if last.value.last.map({ Syllabifier.isConsonant($0) }) ?? false,
+                   $1.first.map({ Syllabifier.startsWithScanConsonant($0.value) }) ?? false {
                     $0[$0.count - 1][$0.last!.count - 1].isLong = true
                 }
                 // Likewise, a case of '-v cc-', produces a similar result
-                else if $1.first?.value.first.map { Syllabifier.isConsonant($0) } ?? false,
+                else if $1.first?.value.first.map({ Syllabifier.isConsonant($0) }) ?? false,
                         let droppedFront = $1.first?.value.dropFirst(),
                         Syllabifier.startsWithScanConsonant(String(droppedFront)) {
                     let isLiquidFlexible = $1.first?.value.first != droppedFront.first && Syllabifier.isLiquid(droppedFront.first ?? " ")
@@ -50,7 +50,7 @@ public struct Scanner {
         // - Vowel elision
         // - 'H' elision (MAYBE?)
         // - '-m' nasalization elision
-        return elide ? result : result.reduce(into: [[Syllable]]()) {
+        return elide ? result.reduce(into: [[Syllable]]()) {
             let performElision = { (r: inout [[Syllable]], w: [Syllable]) in
                 r[r.count - 1][r.last!.count - 1].value += "_\(w.first!.value)"
                 r[r.count - 1][r.last!.count - 1].isLong = w.first!.isLong
@@ -58,34 +58,34 @@ public struct Scanner {
             }
             
             // In the scenario of '-v1 v2-', the v1 gets knocked out and the syllable merges with the v2 one: '-v2-'
-            if let last = $0.last?.last, Syllabifier.isOpen((last.value, false /* dummy value */)), 
+            if let last = $0.last?.last, Syllabifier.isOpen((last.value, false /* dummy value */)),
                let first = $1.first?.value, Syllabifier.startsWithVowel(first) {
                 performElision(&$0, $1)
             }
             // In the scenario of '-v1m v2-', the v1m gets knocked out and the syllable merges with the v2 one: '-v2-'
-            else if let last = $0.last?.last?.value, 
-               last.dropLast().last.map { Syllabifier.isVowel($0) } ?? false, 
+            else if let last = $0.last?.last?.value,
+               last.dropLast().last.map({ Syllabifier.isVowel($0) }) ?? false,
                last.last == "m" || last.last == "M",
                let first = $1.first?.value, Syllabifier.startsWithVowel(first) {
                 performElision(&$0, $1)
-            } 
+            }
             // If none of these elision attempts suceeded, simply append the syllables as is
             else {
                 $0.append($1)
             }
-        }
+        } : result
     }
     
     /// Represents
     public enum Meter {
         /// Dactylic hexameter. This constitutes a mixture of dactyls and spondees within the first five feet, with a final spondee.
         ///
-        /// - Warning: This will fail to match lines such as Aen 1.105 with a single long as the final foot.
+        /// - Warning: This will fail to match lines such as Aen X.X with a single long as the final foot.
         case hexameter
     }
     
     public static func match(line: [String], against meter: Meter) -> Bool {
-        // Convert words to syllables 
+        // Convert words to syllables
         return match(line: Scanner.scan(line: line, elide: true), against: meter)
     }
     
@@ -123,7 +123,7 @@ public struct Scanner {
         case .hexameter:
             var result = applyingTemplates(finalSpondee, to: combined)
             for _ in 0 ..< 5 {
-                guard let unwrapped = result else {
+                guard let unwrapped = result, !unwrapped.isEmpty else {
                     return false
                 }
                 result = applyingTemplates(dactyl, to: unwrapped)
